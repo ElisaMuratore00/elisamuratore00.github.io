65814c7549d266582569ec9a156c2ff5d5b3ed69
ElisaMuratore00 <eli.mduemila@gmail.com>
2026-02-10 11:55:37 +0100

    feat: added shaker talk

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,16 +1,61 @@
 var addressPoints = [
   [
-    "Marked temporal point processes for simulating and capturing coordinated behaviour<br />Conference on Complex Systems CCS 2025; Siena, SI",
-    43.3185536,
-    11.3316533
-  ],
-  [
     "How to design a collection strategy for monitoring disinformation and hate speech in Telegram<br />Conference on Complex Systems CCS 2025; Siena, SI",
     43.3185536,
     11.3316533
   ],
   [
     "Notte della Ricerca 2025 - How science unmasks disinformation<br />Notte della Ricerca 2025; Trento, TN",
+    46.0664228,
+    11.1257601
+  ],
+  [
+    "Marked temporal point processes for simulating and capturing coordinated behaviour<br />Sharing know-how, research lines and career tips: a symposium for early stage researchers; Verona, VR",
+    45.4384958,
+    10.9924122
+  ],
+  [
+    "Marked temporal point processes for simulating and capturing coordinated behaviour<br />Conference on Complex Systems CCS 2025; Siena, SI",
+    43.3185536,
+    11.3316533
+  ],
+  [
+    "Self-exciting temporal point processes: a deep dive into the statistics of Hawkes processes<br />University of Trento, Department of Mathematics; Trento, ITA",
+    46.0680009,
+    11.1207268
+  ],
+  [
+    "Come la scienza smaschera la disinformazione<br />Notte della ricerca, MUSE, Trento, TN; Trento, TN",
+    46.0664228,
+    11.1257601
+  ],
+  [
+    "How to design a collection strategy for monitoring disinformation and hate speech in Telegram<br />Conference on Complex Systems CCS 2025; Siena",
+    43.167206,
+    11.467561
+  ],
+  [
+    "Notte della Ricerca 2025 - How science unmasks disinformation<br />Notte della Ricerca 2025; Trento",
+    46.0664228,
+    11.1257601
+  ],
+  [
+    "Marked temporal point processes for simulating and capturing coordinated behaviour<br />Sharing know-how, research lines and career tips: a symposium for early stage researchers; Verona",
+    45.4424977,
+    10.9857377
+  ],
+  [
+    "Marked temporal point processes for simulating and capturing coordinated behaviour<br />Conference on Complex Systems CCS 2025; Siena",
+    43.167206,
+    11.467561
+  ],
+  [
+    "Self-exciting temporal point processes: a deep dive into the statistics of Hawkes processes<br />University of Trento, Department of Mathematics; Trento",
+    46.0664228,
+    11.1257601
+  ],
+  [
+    "Come la scienza smaschera la disinformazione<br />Notte della ricerca, MUSE, Trento, TN; Trento",
     46.0664228,
     11.1257601
   ]
